4e05448dadd0bc0d4f510a501fd59ba7d8bd7103
Johnny <jly48@ucmerced.edu>
2023-02-04 03:21:39 -0800

    Updated styling for skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useInView } from 'react-intersection-observer';
+import { useInView } from "react-intersection-observer";
 import styles from "./styles/Skills.scss";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -11,39 +11,52 @@ import photography from "../components/assets/photography.png";
 import lightroom from "../components/assets/lightroom.png";
 
 const Skills = () => {
-  const { ref: myRef, inView: visibleElement} = useInView({triggerOnce: true});
+  const { ref: myRef, inView: visibleElement } = useInView({
+    triggerOnce: true,
+  });
 
-   
   return (
     <div className="skills">
       <div className="skills__container">
-        <Container>
+        <Container className="skillsHeading">
           <Row>
-            <Col className={`${'hidden'} ${visibleElement ? 'show' : '' }`} id="languages">
+            <Col
+              className={`${"hidden"} ${visibleElement ? "showH1" : ""}`}
+              id="languages"
+            >
               {" "}
               <h1> Languages </h1>
             </Col>
-            <Col className={`${'hidden'} ${visibleElement ? 'show2' : '' }`} id="tools">
+            <Col
+              className={`${"hidden"} ${visibleElement ? "showH2" : ""}`}
+              id="tools"
+            >
               {" "}
               <h1> Tools/Libraries </h1>
             </Col>
-            <Col className={`${'hidden'} ${visibleElement ? 'show3' : '' }`} id="other">
+            <Col
+              className={`${"hidden"} ${visibleElement ? "showH3" : ""}`}
+              id="other"
+            >
               {" "}
               <h1> Other </h1>
             </Col>
           </Row>
-
+        </Container>
+        <Container>
           <Row>
-            <Col className={`${'hidden'} ${visibleElement ? 'show' : '' }`} id="languages" ref={myRef}>
-              <Col className="logo-container" lg="auto">
+            <Col
+              className={`${"hidden"} ${visibleElement ? "appear" : ""}`}
+              id="languages"
+              ref={myRef}
+            >
+              <Col className="logo-container1" lg="auto">
                 <img
                   className="icon"
                   src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg"
                   alt="HTML/CSS"
                 />
-                HTML/CSS 
-              </Col>
-              <Col className="logo-container" lg="auto">
+                HTML/CSS
                 <img
                   className="jsIcon"
                   src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
@@ -52,63 +65,63 @@ const Skills = () => {
                 JavaScript
               </Col>
 
-              <Col className="logo-container" lg="auto">
+              <Col className="logo-container1" lg="auto">
                 <img
                   className="icon"
                   src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg"
                   alt="Python"
                 />
                 Python
-              </Col>
-              <Col className="logo-container" lg="auto">
                 <img
-                  className="icon"
+                  className="cplusplusIcon"
                   src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg"
                   alt="C/C++"
                 />
                 C/C++
               </Col>
-              <Col className="logo-container" lg="auto">
-                <img className="icon" src={sql} alt="SQL"/>
+
+              <Col className="logo-container1" lg="auto">
+                <img className="icon" src={sql} alt="SQL" />
                 SQL
               </Col>
             </Col>
 
-            <Col className={`${'hidden'} ${visibleElement ? 'show2' : '' }`} id='tools' ref={myRef} xs lg={4}>
-              <Col className="logo-container" lg="auto">
+            <Col
+              className={`${"hidden"} ${visibleElement ? "appear2" : ""}`}
+              id="tools"
+              ref={myRef}
+            >
+              <Col className="logo-container2" lg="auto">
                 <img
                   className="icon"
                   src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
                   alt="React"
                 />
                 React
-              </Col>
-              <Col className="logo-container" lg="auto">
                 <img
-                  className="icon"
+                  className="nodeIcon"
                   src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
                   alt="Node"
                 />
                 Node
               </Col>
-              
-              <Col className="logo-container" lg="auto">
+
+              <Col className="logo-container2" lg="auto">
                 <img
                   className="icon"
                   src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg"
                   alt="Scss"
                 />
                 Scss
-              </Col>
-              <Col className="logo-container" lg="auto">
                 <img
-                  className="icon"
+                  className="bootstrapIcon"
                   src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg"
                   alt="Bootstrap"
                 />
                 Bootstrap
               </Col>
-              <Col className="logo-container" lg="auto">
+
+              <Col className="logo-container2" lg="auto">
                 <img
                   className="icon"
                   src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"
@@ -118,22 +131,31 @@ const Skills = () => {
               </Col>
             </Col>
 
-            <Col className={`${'hidden'} ${visibleElement ? 'show3' : '' }`} id='other' ref={myRef} lg={4}>
-              <Col className="logo-container" lg="auto">
-                <img className="icon" src={agile} alt="AGILE Methodologies"/>
-                AGILE Methodologies
+            <Col
+              className={`${"hidden"} ${visibleElement ? "appear3" : ""}`}
+              id="other"
+              ref={myRef}
+            >
+              <Col className="logo-container3" lg="auto">
+                <img className="icon" src={agile} alt="AGILE Methodologies" />
+                AGILE
+                <img
+                  className="typeIcon"
+                  src={typingSpeed}
+                  alt="Typing Speed"
+                />
+                110+ WPM
               </Col>
-              <Col className="logo-container" lg="auto">
-                <img className="icon" src={typingSpeed} alt="Typing Speed" />
-                Typing Speed: 110+ WPM
-              </Col>
-              <Col className="logo-container" lg="auto">
+
+              <Col className="logo-container3" lg="auto">
                 <img className="icon" src={photography} alt="Photography" />
                 Photography
-              </Col>
-              <Col className="logo-container" lg="auto">
-                <img className="icon" src={lightroom} alt="Adobe Lightroom" />
-                Adobe Lightroom
+                <img
+                  className="lightroomIcon"
+                  src={lightroom}
+                  alt="Adobe Lightroom"
+                />
+                Lightroom
               </Col>
             </Col>
           </Row>
