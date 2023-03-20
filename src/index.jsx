332c2ff58acf4d1713daef9ca72c80f43457e8ba
Johnny <jly48@ucmerced.edu>
2023-03-19 19:33:53 -0700

    added service worker and other items for pwa and updated webp images to be more clear

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {createRoot} from "react-dom/client";
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
 const container = document.getElementById("root");
@@ -13,3 +14,8 @@ root.render(
     </BrowserRouter>,
 );
 
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
