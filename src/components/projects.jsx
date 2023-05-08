c8bc5a69c5e9cf038e68f9e67f5218cca83e9e82
Johnny <jly48@ucmerced.edu>
2023-05-07 20:28:43 -0700

    added new project and disabled pointer events for project tools

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -10,10 +10,157 @@ import website from "../assets/personalWebsite.webp";
 import hackmerced from "../assets/hackmerced.webp";
 import devpost from "../assets/devpost-icon.svg";
 import google from "../assets/google.svg";
+import flyerscan from "../assets/FlyerScan.webp";
 
 function Projects() {
   return (
     <div className="projectsTab">
+      <Container className="project-container">
+        <Row>
+          <Col className="logo-container" lg="auto">
+            <img src={flyerscan} width="100px" alt="" />
+          </Col>
+          <Col lg>
+            <b style={{ fontSize: "20px" }}> FlyerScan </b>
+            <p id="date" style={{ fontSize: "12px" }}>
+              {" "}
+              January 2023 - May 2023{" "}
+            </p>
+            <a
+              className="githubLink"
+              href="https://github.com/LyJohnny/FlyerScan"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <GitHub fontSize="large" style={{ color: "black" }} /> View on
+              GitHub
+            </a>
+            <a
+              className="figmaLink"
+              href="https://www.figma.com/file/sUUVQxGneejPU5kFZnObCc/FlyerScan?type=design&node-id=0%3A1&t=Cz0T2fDih9VlUvkJ-1"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg"
+                width="30px"
+                alt=""
+              />{" "}
+              View on Figma
+            </a>
+
+            <p id="project-description">
+              Progressive Web App designed to help students keep track of
+              on-campus events by scanning event flyers and extracting event
+              details into a personal calendar. Using Optical Character
+              Recognition (OCR) and Named Entity Recognition (NER), the app
+              identifies parameters pertaining to a calendar event on physical
+              flyers and imports the data into the user’s personal calendar app.
+              The user can confirm and edit any necessary changes using the
+              app’s speech detection and keyboard text input.
+            </p>
+          </Col>
+        </Row>
+        <Row>
+          <a
+            className="tool-container"
+            href="/#"
+            onClick={(e) => e.preventDefault()}
+          >
+            <img
+              id="tool-icon2"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flask/flask-original.svg"
+              alt="flask-icon"
+            />
+            Flask
+          </a>
+          <a
+            className="tool-container"
+            href="/#"
+            onClick={(e) => e.preventDefault()}
+          >
+            <img id="tool-icon2" src={google} alt="googlebert-icon" />
+            Google BERT Model
+          </a>
+
+          <a
+            className="tool-container"
+            href="/#"
+            onClick={(e) => e.preventDefault()}
+          >
+            <img
+              id="tool-icon2"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg"
+              alt="html-icon"
+            />
+            HTML
+          </a>
+          <a
+            className="tool-container"
+            href="/#"
+            onClick={(e) => e.preventDefault()}
+          >
+            <img
+              id="tool-icon2"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg"
+              alt="css-icon"
+            />
+            CSS
+          </a>
+
+          <a
+            className="tool-container"
+            href="/#"
+            onClick={(e) => e.preventDefault()}
+          >
+            <img
+              id="tool-icon2"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+              alt="javascript-icon"
+            />
+            JavaScript
+          </a>
+
+          <a
+            className="tool-container"
+            href="/#"
+            onClick={(e) => e.preventDefault()}
+          >
+            <img
+              id="tool-icon2"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg"
+              alt="bootstrap-icon"
+            />
+            Bootstrap
+          </a>
+
+          <a
+            className="tool-container"
+            href="/#"
+            onClick={(e) => e.preventDefault()}
+          >
+            <img
+              id="tool-icon2"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlite/sqlite-original.svg"
+              alt="sqlite-icon"
+            />
+            SQLite
+          </a>
+
+          <a
+            className="tool-container"
+            href="/#"
+            onClick={(e) => e.preventDefault()}
+          >
+            <img
+              id="tool-icon2"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"
+              alt="git-icon"
+            />
+            Git
+          </a>
+        </Row>
+      </Container>
       <Container className="project-container">
         <Row>
           <Col className="logo-container" lg="auto">
@@ -26,7 +173,10 @@ function Projects() {
           <Col lg>
             <b style={{ fontSize: "20px" }}> DevTracker </b>
             <p style={{ fontSize: "15px" }}> Innovate To Grow </p>
-            <p id="date" style={{ fontSize: "12px" }}> September 2022 - December 2022 </p>
+            <p id="date" style={{ fontSize: "12px" }}>
+              {" "}
+              September 2022 - December 2022{" "}
+            </p>
 
             <PosterModal />
 
@@ -132,7 +282,10 @@ function Projects() {
               {" "}
               JPMorgan Chase &amp; Co. Code For Good{" "}
             </p>
-            <p id="date" style={{ fontSize: "12px" }}> February 2022 </p>
+            <p id="date" style={{ fontSize: "12px" }}>
+              {" "}
+              February 2022{" "}
+            </p>
             <p id="project-description">
               Redesigned the user interface of the Hispanic Unity of Florida's
               website to simplify navigation and improve user experience.
@@ -237,7 +390,10 @@ function Projects() {
           <Col lg>
             <b style={{ fontSize: "20px" }}> Project Fliar </b>
             <p style={{ fontSize: "15px" }}>HackMerced</p>
-            <p id="date" style={{ fontSize: "12px" }}> February 2020 </p>
+            <p id="date" style={{ fontSize: "12px" }}>
+              {" "}
+              February 2020{" "}
+            </p>
             <Row>
               <Col className="project-links-container">
                 <a
@@ -260,9 +416,9 @@ function Projects() {
               </Col>
             </Row>
             <p id="project-description">
-              Mobile Application aimed at reducing the time and materials needed
-              for event planning and distribution of flyers. Streamlined the
-              process of inputting event details into a personal Calendar by
+              Android Application aimed at reducing the time and materials
+              needed for event planning and distribution of flyers. Streamlined
+              the process of inputting event details into a personal Calendar by
               using an image-scanning feature to extract data such as event
               name, date, start/end time, and location.
             </p>
@@ -324,7 +480,10 @@ function Projects() {
           </Col>
           <Col lg>
             <b style={{ fontSize: "20px" }}> Personal Website </b>
-            <p id="date" style={{ fontSize: "12px" }}> July 2022 - March 2023 </p>
+            <p id="date" style={{ fontSize: "12px" }}>
+              {" "}
+              July 2022 - March 2023{" "}
+            </p>
             <a
               className="githubLink"
               href="https://github.com/LyJohnny/react-personalwebsite"
